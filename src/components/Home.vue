e21356157d5c6a0f912231309f3d10f0be8153e1
jambo <602975235@qq.com>
2020-02-22 19:05:14 +0800

    edit header

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../../src/assets/logo.jpg" alt="logo">
+        <span>后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主题区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右边内容主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -16,5 +30,34 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+  .home-container {
+    height: 100%;
+  }
+  .el-header {
+    background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+    div {
+      display: flex;
+      align-items: center;
+      span {
+        margin-left: 15px;
+      }
+      img {
+        width: 52px;
+        height: 52px;
+        border-radius: 50%;
+        box-shadow: 0px 0px 4px #fff;
+      }
+    }
+  }
+  .el-aside {
+    background-color: #333744;
+  }
+  .el-main {
+    background: #eaedf1;
+  }
 </style>
