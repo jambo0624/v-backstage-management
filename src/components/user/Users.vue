a7f9da763fe31b2d66169c309181fb0fd3a5be8a
jambo <602975235@qq.com>
2020-02-22 22:40:26 +0800

    el table

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -8,21 +8,54 @@
     </el-breadcrumb>
     <el-card>
       <el-row :gutter="20">
-        <el-col span="7">
+        <el-col :span="7">
           <el-input placeholder="请输入内容">
             <el-button slot="append" icon="el-icon-search"></el-button>
           </el-input>
         </el-col>
-        <el-col span="4">
+        <el-col :span="4">
           <el-button type="primary">添加用户</el-button>
         </el-col>
       </el-row>
+      <el-table :data="userlist" border stripe>
+        <el-table-column label="姓名" prop="username"></el-table-column>
+        <el-table-column label="邮箱" prop="email"></el-table-column>
+        <el-table-column label="电话" prop="mobile"></el-table-column>
+        <el-table-column label="角色" prop="role_name"></el-table-column>
+        <el-table-column label="状态" prop="mg_state"></el-table-column>
+        <el-table-column label="操作"></el-table-column>
+      </el-table>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      // 获取用户列表的参数对象
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 2
+      },
+      userlist: [],
+      total: 0
+    }
+  },
+  created() {
+    this.getUserList()
+  },
+  methods: {
+    async getUserList() {
+      const { data: result } = await this.$http.get('/users', { params: this.queryInfo })
+      if (result.meta.status !== 200) return this.$message.error('获取用户列表失败！')
+      this.userlist = result.data.users
+      this.total = result.data.total
+      console.log(result)
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
