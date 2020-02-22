90976a7957f22d9304e400e2e0da44a8ff957584
jambo <602975235@qq.com>
2020-02-22 20:02:49 +0800

    dynamic get menulist

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
     <!-- 头部区域 -->
     <el-header>
       <div>
-        <img src="../../src/assets/logo.jpg" alt="logo">
+        <img src="../../src/assets/logo.jpg" alt="logo" />
         <span>后台管理系统</span>
       </div>
       <el-button type="info" @click="logout">退出</el-button>
@@ -11,7 +11,28 @@
     <!-- 页面主题区域 -->
     <el-container>
       <!-- 侧边栏 -->
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px">
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <!-- 一级菜单 可折叠，group和item不可折叠-->
+          <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
+            <!-- 一级菜单模板区 -->
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>{{item.authName}}</span>
+            </template>
+            <el-menu-item index="subItem.id+''" v-for="subItem in item.children" :key="subItem.id">
+              <template slot="title">
+                <i class="el-icon-help"></i>
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <!-- 右边内容主体 -->
       <el-main>Main</el-main>
     </el-container>
@@ -20,44 +41,58 @@
 
 <script>
 export default {
+  data() {
+    return {
+      menulist: []
+    }
+  },
+  created() {
+    this.getMenuList()
+  },
   methods: {
-    logout () {
+    logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    async getMenuList() {
+      const { data: res } = await this.$http.get('/menus')
+      if (res.data.meta === 200) return this.$message.error(res.meta.msg)
+      this.menulist = res.data
+      console.log(res.data)
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-  .home-container {
-    height: 100%;
-  }
-  .el-header {
-    background-color: #373d41;
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  div {
     display: flex;
-    justify-content: space-between;
     align-items: center;
-    color: #fff;
-    font-size: 20px;
-    div {
-      display: flex;
-      align-items: center;
-      span {
-        margin-left: 15px;
-      }
-      img {
-        width: 52px;
-        height: 52px;
-        border-radius: 50%;
-        box-shadow: 0px 0px 4px #fff;
-      }
+    span {
+      margin-left: 15px;
+    }
+    img {
+      width: 52px;
+      height: 52px;
+      border-radius: 50%;
+      box-shadow: 0px 0px 4px #fff;
     }
   }
-  .el-aside {
-    background-color: #333744;
-  }
-  .el-main {
-    background: #eaedf1;
-  }
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background: #eaedf1;
+}
 </style>
