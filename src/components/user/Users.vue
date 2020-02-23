f8d67b5116c84d86ccc36ef14b7f90d76f93568d
jambo <602975235@qq.com>
2020-02-23 10:05:27 +0800

    delete user

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -38,7 +38,7 @@
               @click="showEditDialog(scope.row.id)"
             ></el-button>
             <!-- 删除用户 -->
-            <el-button type="danger" icon="el-icon-delete" size="min"></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="min" @click="removeUserById(scope.row.id)"></el-button>
             <!-- 设置角色 -->
             <el-tooltip effect="dark" content="设置角色" placement="top" :enterable="false">
               <el-button type="warning" icon="el-icon-setting" size="min"></el-button>
@@ -243,6 +243,25 @@ export default {
         this.getUserList()
         this.$message.success('更新用户信息成功！')
       })
+    },
+    async removeUserById(id) {
+      // 询问用户是否删除数据
+      const confirmResult = await this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
+      // 确认删除，返回confirm字符串
+      // 取消删除，返回cancel字符串
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('已取消删除')
+      }
+      const { data: res } = await this.$http.delete('users/' + id)
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除用户失败！')
+      }
+      this.$message.success('删除用户成功！')
+      this.getUserList()
     }
   }
 }
