f950752559903377a70283d0b6dab0738dea2df3
jambo <602975235@qq.com>
2020-02-25 11:09:51 +0800

    done report

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params.vue'
 import List from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 
 Vue.use(VueRouter)
 
@@ -75,6 +76,11 @@ const routes = [
         path: '/orders',
         name: 'Order',
         component: Order
+      },
+      {
+        path: '/reports',
+        name: 'Report',
+        component: Report
       }
     ]
   }
