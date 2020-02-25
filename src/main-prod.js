ee452cb02a4c19fdc3bd5ed03dcf2d14ece5aedc
jambo <602975235@qq.com>
2020-02-25 18:36:14 +0800

    lazy-load cdn

--- src/main-prod.js
+++ src/main-prod.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './plugins/element.js'
 
 import '../src/assets/css/global.css'
 import '../src/assets/fonts/iconfont.css'
@@ -10,8 +9,13 @@ import treeTable from 'vue-table-with-tree-grid'
 
 // 导入富文本编辑器
 import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.prototype.$http = axios
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,30 @@
 // vue.config.js
 module.exports = {
-  // 选项...
+  chainWebpack: config => {
+    // 发布模式
+    config.when(process.env.NODE_ENV === 'production',config => {
+      config.entry('app').clear().add('./src/main-prod.js')
+      config.set('externals',{
+        vue: 'Vue',
+        'vue-router': 'VueRouter',
+        axios: 'axios',
+        echarts: 'echarts',
+        nprogress: 'NProgress',
+        quill: 'VueQuillEditor'
+      })
+      config.plugin('html').tap(args => {
+        args[0].isProd = true
+        return args
+      })
+    })
+
+    // 开发模式
+    config.when(process.env.NODE_ENV === 'development',config => {
+      config.entry('app').clear().add('./src/main.js')
+      config.plugin('html').tap(args => {
+        args[0].isProd = false
+        return args
+      })
+    })
+  }
 }
