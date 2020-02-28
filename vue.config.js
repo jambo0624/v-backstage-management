e5a44f23ef0233d73a4121eaa028d8752673e7d9
jambo <602975235@qq.com>
2020-02-28 13:30:03 +0800

    publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 // vue.config.js
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/v-backstage-management/'
+    : '/',
   chainWebpack: config => {
     // 发布模式
     config.when(process.env.NODE_ENV === 'production',config => {
